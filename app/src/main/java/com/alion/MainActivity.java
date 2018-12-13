4e2a637b310a0083e9cc634b6af02746d945faf9
p_jlonliang <p_jlonliang@tencent.com>
2018-12-13 14:42:45 +0800

    添加ViewOutlineProvider实现遮罩

--- app/src/main/java/com/alion/MainActivity.java
+++ app/src/main/java/com/alion/MainActivity.java
@@ -1,14 +1,19 @@
 package com.alion;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.database.ContentObserver;
+import android.graphics.Outline;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.Button;
 
 import com.alion.countdowntimer.TimeCount;
 import com.alion.myapplication.R;
@@ -21,13 +26,27 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
     private ContentObserver observer;
     private Uri uri = Uri.parse("content://.test.a");  ;
-
+    private Button mButton;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.button);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        ViewOutlineProvider viewOutlineProvider1 =
+                new ViewOutlineProvider() {
+
+                    @Override
+                    public void getOutline(View view, Outline outline) {
+                        //修改outline为特定形状
+                        outline.setRoundRect(0, 0,
+                                view.getWidth(),
+                                view.getHeight(), 90);
+                    }
+                };
+        mButton.setOutlineProvider(viewOutlineProvider1);
+       /* findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new TimeCount(500000, 1000, new TimeCount.TimerCallback() {
@@ -44,6 +63,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }).start();
             }
-        });
+        });*/
     }
 }

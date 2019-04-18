dfedbf139c1b4792e2848d0787b66e4564dbc32d
p_jlonliang <p_jlonliang@tencent.com>
2019-04-18 18:12:52 +0800

    添加rvadapter替换原来的quickbaseadapter CommonAdapter MultiItemTypeAdapter HeaderAndFooterWrapper EmptyWrapper

--- app/src/main/java/com/alion/recycleview/MainActivity.java
+++ app/src/main/java/com/alion/recycleview/MainActivity.java
@@ -1,23 +1,16 @@
 package com.alion.recycleview;
 
-import android.annotation.TargetApi;
-import android.database.ContentObserver;
-import android.graphics.Outline;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewOutlineProvider;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.alion.myapplication.R;
-import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +25,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecycle;
-    List<QuickAdapter.BaseViewData> mData;
-    QuickAdapter qa;
+    List<String> mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,28 +36,25 @@ public class MainActivity extends AppCompatActivity {
         mRecycle.setLayoutManager(new GridLayoutManager(this,9,OrientationHelper.HORIZONTAL,true));
         //mRecycle.setAdapter(new NormalAdapter(mData));
         mRecycle.addItemDecoration(new MDGridRvDividerDecoration(this));
-        qa = new QuickAdapter(R.layout.recycletest,mData);
-        qa.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+        mRecycle.setAdapter(new CommonAdapter<String>(this,R.layout.recycletest,mData) {
             @Override
-            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
-                if(view.getId()==R.id.title){
-                    Toast.makeText(getApplicationContext(),"R.id.title",Toast.LENGTH_SHORT).show();
-                }
+            protected void convert(ViewHolder holder, String s, int position) {
+                holder.setText(R.id.title,s)
+                        .setOnClickListener(R.id.title, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.d("alionlog", "onClick: ");
+                            }
+                        })
+                        .setImageResource(R.id.imageicon,R.drawable.ic_lock_redbag);
             }
         });
-        qa.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                Toast.makeText(getApplicationContext(),""+position,Toast.LENGTH_SHORT).show();
-            }
-        });
-        mRecycle.setAdapter(qa);
     }
 
     private void initData() {
-        mData = new ArrayList<QuickAdapter.BaseViewData>();
+        mData = new ArrayList<String>();
         for(int i=100;i<200;i++){
-            mData.add(new QuickAdapter.BaseViewData().setTitle("第"+i+"章"));
+            mData.add("第"+i+"章");
         }
     }
 }
